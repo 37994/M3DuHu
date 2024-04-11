203e84bfc357fa480192a4130f78864516beb969
puck <36668@ma-web.nl>
2024-04-11 16:08:42 +0200

    slider

--- js/script.js
+++ js/script.js
@@ -1,39 +1,33 @@
+let labels = ["A", "B", "C", "D", "E"];
+let data = [100, 80, 40, 65, 30];
 
-let data = {
-    linechart: data = {
-        labels: ["A", "B", "C", "D", "E"],
+const myChart = new Chart(document.getElementById("myChart"), {
+    type: "line",
+    data: {
+        labels,
         datasets: [{
             label: "KwH",
-            data: [100, 80, 40, 65, 30],
+            data,
             backgroundColor: [
                 "rgb(80,80,200)",
                 "rgb(120,120,255)",
                 "rgb(160,160,255)",
                 "rgb(100,100,100)",
                 "rgb(200,200,40)",
-    
+
             ]
         }],
 
     },
-}
-
-let config = {
-    linechart: {
-        type: "line",
-        data: data.linechart,
-        options: {
-            maintainAspectRatio: false,
-            plugins: {
-                legend: {
-                    display: false
-                }
+    options: {
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false
             }
         }
-    },
-}
-
-myChart = new Chart(linechart, config.linechart);
+    }
+});
 
 const spanVanDoos3 = document.getElementById("spanVanDoos3");
 const doos3Button1 = document.getElementById("js--tijdButton1");
