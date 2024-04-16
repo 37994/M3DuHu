a8d79a2e372fd3c0be2e19760ea7703dae83f116
Liam <37994@ma-web.nl>
2024-04-16 11:26:38 +0200

    arduinoCode

--- js/script.js
+++ js/script.js
@@ -169,7 +169,7 @@ let toggles = document.getElementById("js--doos2slider").addEventListener('chang
 //lamp2
 const doos2Checkbox2 = document.getElementById("js--doos2slider2");
 const doos2img2 = document.getElementById("js--imgdoos22");
-let lampisaan2 = false;
+let lampisaan2 = true;
 
 function lampIsAan2() {
     doos2img2.src = "images/lamp-aan.png"
@@ -208,7 +208,54 @@ let toggles3 = document.getElementById("js--doos2slider3").addEventListener('cha
     }
 });
 
+//arduino
 
+const lightEmels = document.getElementsByClassName("light");
+let lights = [false, true, false];
+
+readData("lights.php");
+
+for(let i = 0; i < lights.length; i++) {
+    lightEmels[i].addEventListener('click', ()=>{
+        lights[i] = !lights[i];
+        sendData('lights.php', lights);
+    });
+}
+
+function readData(url) {
+    fetch(url)
+    .then(async (response) => {
+        let json = await response.json();
+        UpdateLightsFromServer(json);
+    })
+}
+
+function sendData(url, data) {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(async (response) => {
+        let json = await response.json();
+        UpdateLightsFromServer(json);
+    })
+}
+
+function UpdateLightsFromServer(json) {
+    lights = json;
+    //console.log(lights);
+    UpdateLightElems(lights);
+}
+
+function UpdateLightElems(data) {
+    for(let i = 0;i < data.length; i++) {
+        if(data[i]) lightEmels[i].style.backgroundColor = 'green';
+        else lightEmels[i].style.backgroundColor = 'red';
+    }
+}
 
 
 liveZonData();
